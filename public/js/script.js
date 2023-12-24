7ac86b31f9c86c88fe186836ca17a192247be4bd
Asio-NOah <asionoah@gmail.com>
2023-12-24 17:58:23 +0300

    24-Dec

--- public/js/script.js
+++ public/js/script.js
@@ -146,6 +146,43 @@ responsive: [
         // instead of a settings object
       ]
     });
+
+    $('.venue-container').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      nextArrow: $('.next'),
+      prevArrow: $('.prev'),
+      responsive: [
+      {
+      breakpoint: 1024,
+      settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+      }
+      },
+      {
+      breakpoint: 900,
+      settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+      }
+      },
+      {
+      breakpoint: 580,
+      settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+      }
+      }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+      ]
+    });
     
   });
 
